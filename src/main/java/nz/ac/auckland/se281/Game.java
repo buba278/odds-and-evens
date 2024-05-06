f854b5e6e22d0c0be0f99fdb97ca49915310126e
buba278 <chrismotta2005@gmail.com>
2024-05-07 09:14:14 +1200

    implemented a playcount field to account for round number within each game

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  // Fields
+  int playCount = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    playCount = 0;
   }
 
-  public void play() {}
+  public void play() {
+    playCount++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(playCount));
+  }
 
   public void endGame() {}
 
