4d5aebf1ea217164b1ba4b0e47959fae8ad7bcab
buba278 <chrismotta2005@gmail.com>
2024-05-11 11:50:21 +1200

    satisfied criteria player and finger print established

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -8,10 +8,12 @@ public class Game {
 
   // Fields
   int playCount = 0;
+  String player = "";
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    player = options[0];
     playCount = 0;
   }
 
@@ -20,12 +22,16 @@ public class Game {
     MessageCli.START_ROUND.printMessage(String.valueOf(playCount));
 
     int fingers = -1;
+    // check with error
     while ((fingers < 0) | (fingers > 5)) {
       MessageCli.INVALID_INPUT.printMessage();
       MessageCli.ASK_INPUT.printMessage();
       String input = Utils.scanner.nextLine();
       fingers = Integer.parseInt(input);
     }
+
+    // hand print if goes through while loop
+    MessageCli.PRINT_INFO_HAND.printMessage(player, String.valueOf(fingers));
   }
 
   public void endGame() {}
