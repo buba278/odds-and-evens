8dd1a4f5b35d22203e8eefcfb5e75f7fde15946a
buba278 <chrismotta2005@gmail.com>
2024-05-13 21:28:47 +1200

    set default behaviour of medium ai to be random strategy with baseline implementation of top after 3 plays

--- src/main/java/nz/ac/auckland/se281/MediumAI.java
+++ src/main/java/nz/ac/auckland/se281/MediumAI.java
@@ -4,13 +4,16 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Choice;
 
 public class MediumAI implements AI {
-  Strategy strategy;
-
+  Strategy strategy = new RandomStrategy();
+  
   @Override
   public void setStrategy(Strategy strategy) {
     this.strategy = strategy;
   }
   public int play(Choice choice, List<Integer> history) {
+    if (history.size() == 4) { // if it passes the 3rd call + only need to do once
+      setStrategy(new TopStrategy());
+    }
     return strategy.getAction(choice);
   }
 }
