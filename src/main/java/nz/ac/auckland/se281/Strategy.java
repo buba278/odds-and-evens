be6fc795f797a3c7916c25206c1b96637465ef44
buba278 <chrismotta2005@gmail.com>
2024-05-16 14:29:54 +1200

    javadocs done for Strategy interface, methods: getAction, and importHistory

--- src/main/java/nz/ac/auckland/se281/Strategy.java
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -4,7 +4,21 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Choice;
 
 public interface Strategy {
+  /**
+   * Returns the given finger amount that the AI will play. 
+   * This is based on the type of Strategy used.
+   * This result is based off of what the aiWin condition is.
+   * 
+   * @param aiWin condition needed for the ai to win
+   * @return int, this is the number of fingers that the AI will play
+   */
   public int getAction(Choice aiWin);
 
+  /**
+   * Imports the history into the strategy. 
+   * This is to account for the previous human plays when doing the top strategy.
+   * 
+   * @param history, the array list accounting for if the player played ODD or EVEN fingers
+   */
   public void importHistory(List<Integer> history);
 }
