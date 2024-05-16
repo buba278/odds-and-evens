00372ffdf47cacd7c87e11e61b0cfd49132aa2f5
buba278 <chrismotta2005@gmail.com>
2024-05-16 15:03:35 +1200

    hard ai javadoc implemented for functionality description

--- src/main/java/nz/ac/auckland/se281/HardAI.java
+++ src/main/java/nz/ac/auckland/se281/HardAI.java
@@ -10,11 +10,24 @@ public class HardAI implements AI {
   private int lastResult = 0;
   private int lastAIPlay = 0;
 
+  /**
+   * This sets the current strategy being used by the HardAI.
+   * This will determine how the HardAI will make its decisions.
+   * 
+   * @param strategy, the current strategy being used by the ai to play its moves.
+   */
   @Override
   public void setStrategy(Strategy strategy) {
     this.strategy = strategy;
   }
 
+  /**
+   * Play the AI's move as in provide the amount of fingers it will play on its turn.
+   * 
+   * @param aiWin, the win condition for the ai so it can make its decisions
+   * @param history, array list of all the previous moves the human made and its "choice"
+   * @return int, it will return the given fingers that the ai will play
+   */
   public int play(Choice aiWin, List<Integer> history) {
     boolean lastWin;
 
