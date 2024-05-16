6c26174d041c66f04cfac8bac7b9eb49d60a1386
buba278 <chrismotta2005@gmail.com>
2024-05-16 15:08:41 +1200

    finished javadocs for AI interface and AIFactory class

--- src/main/java/nz/ac/auckland/se281/AI.java
+++ src/main/java/nz/ac/auckland/se281/AI.java
@@ -4,7 +4,21 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Choice;
 
 public interface AI {
+  
+  /**
+   * This sets the current strategy being used by the EasyAI.
+   * This will determine how the EasyAI will make its decisions.
+   * 
+   * @param strategy, the current strategy being used by the ai to play its moves.
+   */
   public void setStrategy(Strategy strategy);
 
+  /**
+   * Play the AI's move as in provide the amount of fingers it will play on its turn.
+   * 
+   * @param aiWin, the win condition for the ai so it can make its decisions
+   * @param history, array list of all the previous moves the human made and its "choice"
+   * @return int, it will return the given fingers that the ai will play
+   */
   public int play(Choice aiWin, List<Integer> history);
 }
--- src/main/java/nz/ac/auckland/se281/AIFactory.java
+++ src/main/java/nz/ac/auckland/se281/AIFactory.java
@@ -3,6 +3,11 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class AIFactory {
+  /**
+   * This creates instances of different difficulty AIs.
+   * 
+   * @param difficulty, the difficulty at which you want to make an AI for.
+   */
   public static AI createAI(Difficulty difficulty) {
     switch (difficulty) {
       case EASY:
