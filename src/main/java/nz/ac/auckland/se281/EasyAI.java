1ca8761dd4dd4346d1f75453602491e049f10066
buba278 <chrismotta2005@gmail.com>
2024-05-16 14:52:40 +1200

    java docs for the EasyAI fully implemented

--- src/main/java/nz/ac/auckland/se281/EasyAI.java
+++ src/main/java/nz/ac/auckland/se281/EasyAI.java
@@ -6,11 +6,24 @@ import nz.ac.auckland.se281.Main.Choice;
 public class EasyAI implements AI {
   private Strategy strategy = new RandomStrategy();
 
+  /**
+   * This sets the current strategy being used by the EasyAI.
+   * This will determine how the EasyAI will make its decisions.
+   * 
+   * @param strategy, the current strategy being used by the ai to play its moves.
+   */
   @Override
   public void setStrategy(Strategy strategy) {
     // unused for EasyAI
   }
 
+  /**
+   * Play the AI's move as in provide the amount of fingers it will play on its turn.
+   * 
+   * @param choice, the win condition for the ai so it can make its decisions
+   * @param history, array list of all the previous moves the human made and its "choice"
+   * @return int, it will return the given fingers that the ai will play
+   */
   public int play(Choice choice, List<Integer> history) {
     return strategy.getAction(choice);
   }
